1f2a38e85f0638e059699ea9cf5864f4f0eeb0bc
raquel500c <raquel500c@gmail.com>
2017-09-15 12:13:08 +0200

    Iteration #4: Become a Launderer -corregido error

--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -3,10 +3,19 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+    return;
+  }
+
+  res.redirect('/login');
+});
 
 router.get('/dashboard', (req, res, next) => {
   res.render('laundry/dashboard');
 });
+
 
 
 module.exports = router;
